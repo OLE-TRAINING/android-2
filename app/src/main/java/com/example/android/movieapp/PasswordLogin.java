85ff186bf27ae8529916d4a18221844d9e44f8ef
marcelofilhozup <marcelo.filho.zup>
2019-01-28 18:16:46 -0200

    login screen, new account, password login, token validation

--- app/src/main/java/com/example/android/movieapp/PasswordLogin.java
+++ app/src/main/java/com/example/android/movieapp/PasswordLogin.java
@@ -3,11 +3,12 @@ package com.example.android.movieapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PasswordLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_password_login);
+        getSupportActionBar().hide();
     }
 }
---
+++ app/src/main/java/com/example/android/movieapp/TokenValidation.java
@@ -0,0 +1,14 @@
+package com.example.android.movieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TokenValidation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_token_validation);
+        getSupportActionBar().hide();
+    }
+}
